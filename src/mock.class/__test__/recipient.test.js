48c45240d3c1adc38f40c1ad1a9ce1f23b3b208c
Jinrong He <hjr2027@163.com>
2020-11-15 18:14:11 +0800

    [hejinrong] finish

--- src/mock.class/__test__/recipient.test.js
+++ src/mock.class/__test__/recipient.test.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-unresolved
 import Recipient from "../Recipient";
 
 describe("acceptInjection", () => {
@@ -10,7 +11,7 @@ describe("acceptInjection", () => {
     jest.doMock("../covid19Vaccine", () => {
       return jest.fn().mockImplementation(() => {
         return {
-          composition: [],
+          composition: ["Sugar"],
         };
       });
     });
@@ -38,5 +39,6 @@ describe("acceptInjection", () => {
 
     recipient.acceptInjection(new Covid19Vaccine());
     expect(recipient.hasAntibodies).toBe(true);
+    expect(recipient.getHasAntibodies()).toBe(true);
   });
 });
